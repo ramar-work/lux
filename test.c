2f2f5a7577ca75cd95cce296cf55ab63c2d5adcb
Antonio R. Collins II <ramar.collins@gmail.com>
2019-07-22 21:32:29 -0400

    tests have been redefined.  C threads were just too hard for me to get done today.

--- test.c
+++ test.c
@@ -7,8 +7,8 @@
 const char *cmds[] = {
 	//Simple GETs
 //	"curl -i --form name=John --form shoesize=11 http://localhost:2000?yes=bounce&twelve=kittens"
-	"wget --post-data 'name=John&shoesize=111' 'http://localhost:2000?yes=bounce&twelve=kittens'"
-//, "wget http://localhost:2000"
+  "wget -O- http://localhost:2000"
+, "wget -O- --post-data 'name=John&shoesize=111' 'http://localhost:2000?yes=bounce&twelve=kittens'"
 //, "chromium http://localhost:2000"
 
 #if 0
@@ -73,21 +73,59 @@ int main (int argc, char *argv[]) {
 		}
 	}
 #endif
-	//
+
+	const char testdir[] = "/tmp/hypnotests";
+	int index = 0;
 	char **cmd = (char **)cmds;	
+
+#if 0
+	//mkdir a test directory of some sort
+	if ( mkdir( testdir, S_IRWXU ) == -1 ) {
+		fprintf( stderr, "mkdir error: %s\n", strerror (errno) );
+		return 1;
+	}
+#endif
+
 	while ( *cmd ) {
-		FILE *p = popen( *cmd, "r" );
+		//clone the command, add your own redirect rules
+		char newcmd[ 1024 ];
+		memset( newcmd, 0, sizeof( newcmd ) );
+		snprintf( newcmd, sizeof( newcmd ) - 1, "%s 2>/dev/null >%s/%d", *cmd, testdir, index ); 
+
+		//run the new process
+		FILE *p = popen( newcmd, "r" );
 		if ( !p )
 			fprintf( stderr, "Couldn't execute sequence: '%s' - %s\n", *cmd, strerror(errno) );
 		else {
-			char buf[ 120000 ];
-			memset( buf, 0, sizeof(buf) );
+			//char buf[ 120000 ];
+			//memset( buf, 0, sizeof(buf) );
 			//read and extract from the fifo?
 			//fread( p );
 			//close the fifo
+			//wait until it's done?
 			pclose( p );
 		}
 		cmd++;
 	}
+
+	//We need to wait until each of these is done before moving...
+	//(but not too long...)
+
+	//Now, open each file put the message in a buffer, and 
+	//mark the httpstatus, command status and more
+	//cmdstatus | httpstatus | message (expected) | message (recvd) | time to read complete message / early termination
+	cmd = (char **)cmds;	
+	while ( *cmd ) {
+		fprintf( stderr, "*cmd was: %s\n", *cmd );
+		//Couple ways of doing this
+	#if 0
+		//Save to SQLite, and read stuff off
+	#endif	
+	#if 0
+		//Save to simple text delimited file (can pipe to markdown or something else)
+	#endif	
+		cmd++;
+	}
+	
 	return 0;
 }	
