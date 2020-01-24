de893a93b6cafe722158cdb201608169d1fb2d0b
Antonio R. Collins II <ramar.collins@gmail.com>
2020-01-24 09:24:36 -0500

    This is it... It finally works.

--- counter.c
+++ counter.c
@@ -25,34 +25,31 @@ int main (int argc, char *argv[]) {
 		IntSet **intset = NULL;
 		intset = malloc( sizeof(IntSet *) * p );
 		memset( intset, 0, sizeof(IntSet *) * p );
-
-		//Copy into memory 
 		for ( int i=0; i<p; i++ ) {
 			intset[ i ] = &ii[ i ];
 		}
-			
+	
 		IntSet **w = intset;
 		int c = 0;
-		printf( "%d = [", p );
+		//printf( "%d = [", p );
 		while ( (*w)->a < (*w)->b ) {
 			//Show me the status of the line in question
-			printf( "\n  %p => ", w ); TABS(c); printf( "%d, ", (*w)->a );
+			//printf( "\n  %p => ", w ); TABS(c); printf( "%d, ", (*w)->a );
 	
 			//Move to the next block or build a sequence
 			if ( c < (p - 1) ) {
 				w++, c++;
-				printf( "[UP] from %d - %d =? %d", c-1, c, (*w)->a, (*w)->b );
+				//printf( "[UP] from %d - %d =? %d", c-1, c, (*w)->a, (*w)->b );
 				continue;
 			}
 			
 			//Print a sequence
-			printf( "[HIGHEST] => sequence " );
+			//printf( "[HIGHEST] => sequence " );
 			for ( int i=0; i<p; i++ ) {
 				printf( "%d,", intset[i]->a );
 			}
 
 			//Increment the number 
-	#if 1
 			while ( 1 ) {
 				(*w)->a++;
 				//printf( "L%d %d == %d, STOP", c, (*w)->a, (*w)->b );
@@ -67,35 +64,9 @@ int main (int argc, char *argv[]) {
 					}
 				}
 			}
-	#else
-			(*w)->a++;
-			if ( c && ( (*w)->a == (*w)->b ) ) {
-				printf("\n");TABS( (c + 2) ); printf( "[DOWN] from %d to ", c );
-				(*w)->a = 0;
-				while ( c ) {
-					c--, w--;
-					printf( "%d w=%p %d == %d - ", c, w, (*w)->a, (*w)->b ); 
-					if ( (*w)->a == (*w)->b ) {
-						(*w)->a = 0;
-					}
-					else {
-						//At this second check, we could still be wrong
-						(*w)->a++;
-						if ( (*w)->a == (*w)->b ) {
-							printf( "Level %d, Element is %d\n", c, (*w)->a );
-							//(*w)->a = 0;
-							continue;
-						}
-						break;
-					}
-				}
-				printf( "%d, ", c );
-			}
-	#endif
-
 		}
 		(*w)->a = 0;
-		printf( "\n]\n" );
+		//printf( "\n]\n" );
 	}
 
 	return 0;
